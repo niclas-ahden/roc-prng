a7d41c5b04fb37c6ef015ea9c20b17f3485f8131
niclas <niclas.ahden@gmail.com>
2026-09-17 17:06:27 +0200

    Latest Roc

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     # The Roc compiler revision, keep the `?dir=src` at the end
-    roc-src.url = "github:roc-lang/roc/9221bca8dd5e2f6b2414f043f6089d783fcbec06?dir=src";
+    roc-src.url = "github:roc-lang/roc/a434ea34de8d3d0742da5ad64f819da880134fce?dir=src";
     roc-nix = {
       url = "github:niclas-ahden/roc-nix";
       inputs.nixpkgs.follows = "nixpkgs";
